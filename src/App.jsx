42817cb7e7ac9e457758b0313c5ff8984a06d957
DevelopWebAdnan <adnaniqbal023@yahoo.com>
2026-06-21 22:42:24 +0600

    Setup Routes, Layout and Footer.

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,6 @@ function App() {
 
   return (
     <>
-      <button className="btn btn-dash">Default</button>
-      <button className="btn btn-dash btn-primary">Primary</button>
-      <button className="btn btn-dash btn-secondary">Secondary</button>
-      <button className="btn btn-dash btn-accent">Accent</button>
-      <button className="btn btn-dash btn-info">Info</button>
-      <button className="btn btn-dash btn-success">Success</button>
-      <button className="btn btn-dash btn-warning">Warning</button>
-      <button className="btn btn-dash btn-error">Error</button>
     </>
   )
 }
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,18 @@
+import {
+  createBrowserRouter,
+} from "react-router-dom";
+import Root from "../Layout/Root/Root";
+import Home from "../Layout/Home/Home/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+        {
+            path: '/',
+            element: <Home></Home>
+        }
+    ]
+  },
+]);
