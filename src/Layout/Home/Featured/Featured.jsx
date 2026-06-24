f05b4f62b6b1610cc0e89918ceaa02c4e60580bc
DevelopWebAdnan <adnaniqbal023@yahoo.com>
2026-06-24 12:02:18 +0600

    Created parallax and added testimonials using React Swiper

--- src/Layout/Home/Featured/Featured.jsx
+++ src/Layout/Home/Featured/Featured.jsx
@@ -5,12 +5,12 @@ import "./Featured.css";
 
 const Featured = () => {
     return (
-        <div className="featured-item text-white py-8 my-20">
+        <div className="featured-item bg-fixed text-white py-8 my-20">
             <SectionTitle
             heading="Featured Items"
             subHeading="Check it out"
             ></SectionTitle>
-            <div className="md:flex items-center justify-center pt-12 pb-20 px-36">
+            <div className="md:flex items-center justify-center bg-slate-500 opacity-60 pt-12 pb-20 px-36">
                 <div>
                     <img src={featuredImg} alt="featured image" />
                 </div>
@@ -18,7 +18,7 @@ const Featured = () => {
                     <p>Aug, 20, 2030</p>
                     <p className="uppercase">What can i do to have it?</p>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi deleniti ratione aliquam, sapiente sed voluptas quia dolorem consectetur delectus est enim laudantium possimus harum id velit eveniet tempora, a cupiditate? Eligendi perspiciatis dignissimos, eveniet cum ut doloribus veritatis explicabo beatae alias blanditiis natus molestias ex, sapiente id! Possimus, repudiandae fugit.</p>
-                    <button className="btn btn-outline">Order Now</button>
+                    <button className="btn btn-outline border-0 border-b-4 mt-4">Order Now</button>
                 </div>
             </div>
         </div>
