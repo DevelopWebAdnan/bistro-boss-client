adf3831ade0a63380345c85e0b05e3a3d787d982
DevelopWebAdnan <adnaniqbal023@yahoo.com>
2026-06-25 14:34:42 +0600

    Displayed different menu items category with cover.

--- src/Layout/Home/PopularMenu/PopularMenu.jsx
+++ src/Layout/Home/PopularMenu/PopularMenu.jsx
@@ -8,14 +8,6 @@ const PopularMenu = () => {
     const [menu] = useMenu();
     const popular = menu.filter(item => item.category === "popular");
 
-    // useEffect(() => {
-    //     fetch('menu.json')
-    //         .then(res => res.json())
-    //         .then(data => {
-    //             const popularItems = data.filter(item => item.category === "popular");
-    //             setMenu(popularItems)
-    //         })
-    // }, [])
     return (
         <section className="mb-12">
             <SectionTitle
--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,14 +1,37 @@
 import { Helmet } from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
 import menuImg from '../../../src/assets/menu/menu-bg.jpg'
+import dessertImg from '../../../src/assets/menu/dessert-bg.jpeg'
+import pizzaImg from '../../../src/assets/menu/pizza-bg.jpg'
+import saladImg from '../../../src/assets/menu/salad-bg.jpg'
+import soupImg from '../../../src/assets/menu/soup-bg.jpg'
+import MenuCategory from './MenuCategory/MenuCategory';
+import useMenu from '../../hooks/useMenu';
+import SectionTitle from '../../components/SectionTitle/SectionTitle';
 
 const Menu = () => {
+
+    const [menu] = useMenu();
+    const offered = menu.filter(item => item.category === "offered");
+    const desserts = menu.filter(item => item.category === "dessert");
+    const pizzas = menu.filter(item => item.category === "pizza");
+    const salads = menu.filter(item => item.category === "salad");
+    const soups = menu.filter(item => item.category === "soup");
     return (
         <div>
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
             <Cover img={menuImg} title="Our menu"></Cover>
+            <SectionTitle
+                subHeading="Don't Miss"
+                heading="today's menu"
+            ></SectionTitle>
+            <MenuCategory items={offered}></MenuCategory>
+            <MenuCategory items={desserts} img={dessertImg} title="dessert"></MenuCategory>
+            <MenuCategory items={pizzas} img={pizzaImg} title="pizza"></MenuCategory>
+            <MenuCategory items={salads} img={saladImg} title="salad"></MenuCategory>
+            <MenuCategory items={soups} img={soupImg} title="soup"></MenuCategory>
         </div>
     );
 };
