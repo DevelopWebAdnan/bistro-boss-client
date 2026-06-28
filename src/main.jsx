ac1d3c94f9f7d8d2173db97f6e50bbdcf7afc043
DevelopWebAdnan <adnaniqbal023@yahoo.com>
2026-06-28 16:14:57 +0600

    Firebase project has been setup.

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,16 @@ import {
 } from "react-router-dom";
 import { router } from './Routes/Routes';
 import { HelmetProvider } from 'react-helmet-async';
+import { AuthContext } from './providers/AuthProvider';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HelmetProvider>
-      <div className='max-w-7xl mx-auto'>
-        <RouterProvider router={router} />
-      </div>
-    </HelmetProvider>
+    <AuthContext>
+      <HelmetProvider>
+        <div className='max-w-7xl mx-auto'>
+          <RouterProvider router={router} />
+        </div>
+      </HelmetProvider>
+    </AuthContext>
   </StrictMode>,
 )
