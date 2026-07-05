162b0c557557a6f1debb4d0e2dad0634369a173d
DevelopWebAdnan <adnaniqbal023@yahoo.com>
2026-07-05 20:06:32 +0600

    Deleted cart item and synced data using refetch

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,7 +1,10 @@
 import { FaAd, FaCalendar, FaHome, FaSearch, FaShoppingCart } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
+import useCart from "../hooks/useCart";
 
 const Dashboard = () => {
+    const [cart] = useCart();
+
     return (
         <div className="flex">
             {/* dashboard side bar */}
@@ -20,7 +23,7 @@ const Dashboard = () => {
                     <li>
                         <NavLink to="/dashboard/cart">
                             <FaShoppingCart></FaShoppingCart>
-                            My Cart</NavLink>
+                            My Cart ({cart.length})</NavLink>
                     </li>
                     <li>
                         <NavLink to="/dashboard/review">
